2e7f99f35577d0e4b6882072868b2669944fafdc
naokiweblog <nk1110.chiba@gmail.com>
2020-09-29 08:45:13 +0900

    migrattion users groups

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('hello', function () {
+    return '<html><body><h1>あああ</h1></body></html>';
+});
